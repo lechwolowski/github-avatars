27efccc8bd2d6ee9d503a0e5493768634e55a7db
lechwolowski <lech.wolowski@gmail.com>
2020-08-01 23:23:30 +0200

    implemented avatar slider

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			data: null,
+			img_url: "",
+			current: 0,
+		};
+	}
+
+	users = ["gaearon", "acdlite", "yyx990803", "unclebob", "martinfowler"];
+
+	componentDidMount() {
+		this.load_img(this.users[this.state.current]);
+	}
+
+	load_img = async user => {
+		fetch(`https://api.github.com/users/${user}`)
+			.then(response => response.json())
+			.then(data => data.avatar_url)
+			.then(url => this.setState({ img_url: url }));
+	};
+
+	next = () => {
+		let new_user =
+			this.state.current < this.users.length - 1 ? this.state.current + 1 : 0;
+		this.setState({ current: new_user, img_url: "" }, () =>
+			this.load_img(this.users[this.state.current])
+		);
+	};
+
+	previous = () => {
+		let new_user =
+			this.state.current > 0 ? this.state.current - 1 : this.users.length - 1;
+		this.setState({ current: new_user, img_url: "" }, () =>
+			this.load_img(this.users[this.state.current])
+		);
+	};
+
+	render() {
+		return (
+			<div>
+				<img src={this.state.img_url} alt="Loading..." /> <br />
+				<button onClick={this.previous}>Previous</button>
+				<button onClick={this.next}>Next</button>
+			</div>
+		);
+	}
 }
 
 export default App;
